e492d887a03637318b7c68b2682363af52f4f715
colefriedlaender <colefriedlaender@outlook.de>
2021-01-26 14:47:08 +0100

    start card

---
+++ src/components/card/card.stories.js
@@ -0,0 +1,11 @@
+import "./card.css";
+import offers from "./card-offers.html";
+import sriLanken from "./card-sriLanken.html";
+
+export default {
+  title: "Components/Typography",
+  parameters: { layout: "centered" },
+};
+
+export const offers = () => offers;
+export const Sri = () => sriLanken;
