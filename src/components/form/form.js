4e1d515f34c0d25e58eb4867b8f0e8ac33935487
colefriedlaender <colefriedlaender@outlook.de>
2021-02-01 15:15:18 +0100

    form

---
+++ src/components/form/form.js
@@ -0,0 +1,11 @@
+export function createRegisterForm() {
+    return `
+        <form class="form">
+          <h2>Reset Password</h2>
+          <p>Please enter your email to receive a link to create a new password via email</p>
+          <input placeholder="E-Mail" />
+          <input type="submit" />
+        </form>
+      `;
+  }
+  
