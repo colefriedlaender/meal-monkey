0c4d8ac8e56d480f308ca05722546bb0f56508b8
colefriedlaender <colefriedlaender@outlook.de>
2021-02-02 17:59:31 +0100

    add

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,11 +1,22 @@
 export function createRegisterForm() {
-    return `
-        <form class="form">
-          <h2>Reset Password</h2>
-          <p>Please enter your email to receive a link to create a new password via email</p>
-          <input placeholder="E-Mail" />
-          <input type="submit" />
-        </form>
-      `;
+    const form =document.createElement("form")
+    form.className = "form"
+    
+    const button = document.createElement("button");
+    button.classList.add("btn")
+    button.innerText = "Sign In";
+
+    function login(){
+        alert("Loged In");
+    }
+    button.addEventListener("click", login,);
+    
+    const title =document.createElement("h2")
+    title.innerText = "Reset Password"
+    
+    const hint =document.createElement("h2")
+    hint.innerText = "Please enter your Email to reset yout Password"
+
+    form.append(title, hint, button)
+    return form;
   }
-  
