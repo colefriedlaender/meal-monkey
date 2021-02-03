cd3e0a44548bde6af25480260d2be42da77b1a4e
colefriedlaender <colefriedlaender@outlook.de>
2021-02-03 13:30:50 +0100

    leon version

--- src/components/form/verify-form.js
+++ src/components/form/verify-form.js
@@ -1,46 +1,54 @@
 function createOTPInputElement() {
-  const password = document.createElement("input");
-  password.type = "password";
-  password.placeholder = "*";
-  password.className = "input";
-  password.maxLength = "1";
+  const input = document.createElement("input");
+  input.type = "password";
+  input.placeholder = "*";
+  input.className = "input";
+  input.maxLength = "1";
 
-  return password;
+  return input;
 }
 
-export function createVerifyForm() {
-  const verify = document.createElement("verify");
-  verify.className = "verify";
-
-  const headline = document.createElement("h2");
-  headline.innerText = "We have send an OTP to your mobile";
-
-  const subline = document.createElement("subline");
-  subline.innerText =
-    "Please check your mobile number 071*****12 continue to reset your password";
-
-  // Password
-  const allinputs = document.createElement("allinputs");
+function createPasswordContainerElement() {
+  const passwordContainer = document.createElement("div");
+  passwordContainer.className = "form__otp";
 
   const otpOne = createOTPInputElement();
   const otpTwo = createOTPInputElement();
   const otpThree = createOTPInputElement();
   const otpFour = createOTPInputElement();
 
-  allinputs.append(otpOne, otpTwo, otpThree, otpFour);
+  passwordContainer.append(otpOne, otpTwo, otpThree, otpFour);
 
-  // Button
-  const button = document.createElement(button);
-  button.className = "btn";
+  return passwordContainer;
+}
+
+export function createVerifyForm() {
+  const form = document.createElement("form");
+  form.className = "form";
+
+  const title = document.createElement("h2");
+  title.innerText = "We have sent an OTP to your Mobile";
+
+  const text = document.createElement("p");
+  text.innerText =
+    "Please check your mobile number 071*****12 continue to reset your password";
+
+  const passwordContainer = createPasswordContainerElement();
+
+  const button = document.createElement("button");
   button.innerText = "Next";
-  function reset() {
-    alert("Loged In");
-  }
-  button.addEventListener("click", reset);
+  button.className = "btn";
 
-  const end = document.createElement("bottum");
-  end.innerText = "Didnt Receive?";
+  const hint = document.createElement("p");
+  hint.innerText = "Didn't Receive?";
+  hint.className = "form__hint";
 
-  verify.append(headline, subline, allinputs, button, end);
-  return verify;
+  const resendLink = document.createElement("a");
+  resendLink.innerText = "Click Here";
+  resendLink.href = "#";
+
+  hint.append(resendLink);
+
+  form.append(title, text, passwordContainer, button, hint);
+  return form;
 }
